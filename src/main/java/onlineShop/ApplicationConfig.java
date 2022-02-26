a2a0476b7af02c3779d306b553a73c653fffad3b
Maggie332 <62532057+Maggie332@users.noreply.github.com>
2022-02-26 09:55:20 -0800

    Update ApplicationConfig.java

--- src/main/java/onlineShop/ApplicationConfig.java
+++ src/main/java/onlineShop/ApplicationConfig.java
@@ -25,7 +25,7 @@ public class ApplicationConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://db-instance-2021spring.xxxxxxxx.us-west-2.rds.amazonaws.com:3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
+        dataSource.setUrl("jdbc:mysql://xxxxxxxx.xxxxxxxx.us-west-2.rds.amazonaws.com:3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
         dataSource.setUsername("xxxx");
         dataSource.setPassword("xxxxxxxx");
 
